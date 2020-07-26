72a2bca372a7753842fa17994bd29aa76aaeb146
Raynnon <assante.f@gmail.com>
2020-07-26 16:37:36 +0000

    changed homepage

---
+++ src/Hello.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Hello.css';
+
+class Hello extends React.Component {
+	render() {
+    	return (
+    		<div>
+    			<h1>Hello World!</h1>
+    			<p>Hola todos</p>
+    		</div>
+    	);
+	}
+}
+
+export default Hello;

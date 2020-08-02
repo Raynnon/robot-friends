b4ed6dad23e5fd79f623d17367d37692fe7fcde7
Raynnon <assante.f@gmail.com>
2020-08-02 13:03:31 +0200

    updated searchbox

--- src/App.js
+++ src/App.js
@@ -12,12 +12,20 @@ class App extends Component {
         }
     }
 
+    onSearchChange = (event) => {
+        this.setState({ searchfield: event.target.value });
+    }
+
     render() {
+        const filteredRobots = this.state.robots.filter(robots => {
+            return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        });
+
         return (
             <div className='tc'>
                 <h1>RoboFriends</h1>
-                <Searchbox />
-                <CardList robots={this.state.robots} />
+                <Searchbox searchChange={this.onSearchChange} />
+                <CardList robots={filteredRobots} />
             </div>
         );
     }
--- src/Searchbox.js
+++ src/Searchbox.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const Searchbox = () => {
+const Searchbox = ({ searchField, searchChange }) => {
     return (
         <div className='pa2'>
-            <input className='pa3 ba b--green bg-lightest-blue' type='search' placeholder='search robots' />
+            <input className='pa3 ba b--green bg-lightest-blue' type='search' placeholder='search robots' onChange={searchChange} />
         </div>
     );
 }
